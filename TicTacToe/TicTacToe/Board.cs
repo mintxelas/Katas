eaad71c0f018060fd0c7404d2d8ac9a3360e3722
mintxelas <fermin.saez@gmail.com>
2022-04-12 08:21:45 +0200

    refact: max two instance variables per class

--- TicTacToe/TicTacToe/Board.cs
+++ TicTacToe/TicTacToe/Board.cs
@@ -1,26 +1,82 @@
+using System;
 using System.Collections.Generic;
 
 namespace TicTacToe;
 
 internal class Board
 {
-    private const int Size = 9;
-    private readonly Dictionary<BoardPosition, Symbol> positions = new(Size);
-    public bool Full { get; private set; }
+    private readonly Dictionary<BoardPosition, Symbol> positions = new(Enum.GetValues(typeof(BoardPosition)).Length);
+    public Symbol? LastSymbolPlayed { get; private set; }
 
-    public bool Contains(BoardPosition position)
+    public bool IsFull()
     {
-        return positions.ContainsKey(position);
+        return positions.Count == Enum.GetValues(typeof(BoardPosition)).Length;
     }
 
     public void Set(BoardPosition position, Symbol symbol)
     {
+        if (positions.ContainsKey(position)) throw new CannotPlaceOnOccupiedPositionException();
+
         positions[position] = symbol;
-        Full = positions.Count == Size;
+        LastSymbolPlayed = symbol;
     }
 
-    public Symbol? At(BoardPosition position)
+    public bool ThreeInRow()
+    {
+        return Horizontally() || Vertically() || TopToBottomDiagonal() || BottomToTopDiagonal();
+    }
+
+    private bool BottomToTopDiagonal()
+    {
+        return
+            At(BoardPosition.Position2) == LastSymbolPlayed &&
+            At(BoardPosition.Position4) == LastSymbolPlayed &&
+            At(BoardPosition.Position6) == LastSymbolPlayed;
+    }
+
+    private bool TopToBottomDiagonal()
+    {
+        return
+            At(BoardPosition.Position0) == LastSymbolPlayed &&
+            At(BoardPosition.Position4) == LastSymbolPlayed &&
+            At(BoardPosition.Position8) == LastSymbolPlayed;
+    }
+
+    private bool Vertically()
+    {
+        return
+            (At(BoardPosition.Position0) == LastSymbolPlayed &&
+             At(BoardPosition.Position3) == LastSymbolPlayed &&
+             At(BoardPosition.Position6) == LastSymbolPlayed)
+            ||
+            (At(BoardPosition.Position1) == LastSymbolPlayed &&
+             At(BoardPosition.Position4) == LastSymbolPlayed &&
+             At(BoardPosition.Position7) == LastSymbolPlayed)
+            ||
+            (At(BoardPosition.Position2) == LastSymbolPlayed &&
+             At(BoardPosition.Position5) == LastSymbolPlayed &&
+             At(BoardPosition.Position8) == LastSymbolPlayed);
+    }
+
+    private bool Horizontally()
+    {
+        return
+            (At(BoardPosition.Position0) == LastSymbolPlayed &&
+             At(BoardPosition.Position1) == LastSymbolPlayed &&
+             At(BoardPosition.Position2) == LastSymbolPlayed)
+            ||
+            (At(BoardPosition.Position3) == LastSymbolPlayed &&
+             At(BoardPosition.Position4) == LastSymbolPlayed &&
+             At(BoardPosition.Position5) == LastSymbolPlayed)
+            ||
+            (At(BoardPosition.Position6) == LastSymbolPlayed &&
+             At(BoardPosition.Position7) == LastSymbolPlayed &&
+             At(BoardPosition.Position8) == LastSymbolPlayed);
+    }
+
+    private Symbol? At(BoardPosition position)
     {
         return positions.ContainsKey(position) ? positions[position] : null;
     }
+
 }
--- TicTacToe/TicTacToe/Game.cs
+++ TicTacToe/TicTacToe/Game.cs
@@ -3,88 +3,27 @@ namespace TicTacToe;
 public class Game
 {
     private readonly Board board = new();
-    private Symbol? lastSymbolPlayed;
     public GameResult Result { get; private set; } = GameResult.Playing;
     
     public void Place(BoardPosition position, Symbol symbol)
     {
         if (Result != GameResult.Playing) throw new GameAlreadyFinishedException();
-        if (!lastSymbolPlayed.HasValue && symbol != Symbol.X) throw new MustBeginGameWithPlayerXException();
-        if (lastSymbolPlayed == symbol) throw new PlayersMustAlternateException();
-        if (Taken(position)) throw new CannotPlaceOnOccupiedPositionException();
-        Occupy(position, symbol);
-        lastSymbolPlayed = symbol;
+        if (!board.LastSymbolPlayed.HasValue && symbol != Symbol.X) throw new MustBeginGameWithPlayerXException();
+        if (board.LastSymbolPlayed == symbol) throw new PlayersMustAlternateException();
+        board.Set(position, symbol);
         CalculateGameResult();
     }
 
     private void CalculateGameResult()
     {
-        if (Horizontally() || Vertically() || TopToBottomDiagonal() || BottomToTopDiagonal())
+        if (board.ThreeInRow())
         {
-            Result = lastSymbolPlayed == Symbol.X ? GameResult.XWins : GameResult.OWins;
+            Result = board.LastSymbolPlayed == Symbol.X ? GameResult.XWins : GameResult.OWins;
         }
 
-        if (Result == GameResult.Playing && board.Full)
+        if (Result == GameResult.Playing && board.IsFull())
         {
             Result = GameResult.Draw;
         }
-    }
-
-    private bool BottomToTopDiagonal()
-    {
-        return
-            board.At(BoardPosition.Position2) == lastSymbolPlayed &&
-            board.At(BoardPosition.Position4) == lastSymbolPlayed &&
-            board.At(BoardPosition.Position6) == lastSymbolPlayed;
-    }
-
-    private bool TopToBottomDiagonal()
-    {
-        return
-            board.At(BoardPosition.Position0) == lastSymbolPlayed &&
-            board.At(BoardPosition.Position4) == lastSymbolPlayed &&
-            board.At(BoardPosition.Position8) == lastSymbolPlayed;
-    }
-
-    private bool Vertically()
-    {
-        return
-            (board.At(BoardPosition.Position0) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position3) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position6) == lastSymbolPlayed)
-            ||
-            (board.At(BoardPosition.Position1) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position4) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position7) == lastSymbolPlayed)
-            ||
-            (board.At(BoardPosition.Position2) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position5) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position8) == lastSymbolPlayed);
-    }
-
-    private bool Horizontally()
-    {
-        return
-            (board.At(BoardPosition.Position0) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position1) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position2) == lastSymbolPlayed)
-            ||
-            (board.At(BoardPosition.Position3) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position4) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position5) == lastSymbolPlayed)
-            ||
-            (board.At(BoardPosition.Position6) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position7) == lastSymbolPlayed &&
-             board.At(BoardPosition.Position8) == lastSymbolPlayed);
-    }
-
-    private void Occupy(BoardPosition position, Symbol symbol)
-    {
-        board.Set(position, symbol);
-    }
-
-    private bool Taken(BoardPosition position)
-    {
-        return board.Contains(position);
     }
 }
